b9cd3b2b1926ed04d47adf69ee22ad17ad2864cb
samgator <skaterpro58@icloud.com>
2025-02-17 14:38:29 -0500

    match original code style

--- lib/hooks/useDarkMode.ts
+++ lib/hooks/useDarkMode.ts
@@ -2,26 +2,25 @@ import { useEffect } from 'preact/hooks';
 import { useSignal } from '@preact/signals';
 
 export function useDarkMode() {
-  const darkMode = useSignal(
-    window.matchMedia('(prefers-color-scheme: dark)').matches,
-  );
+  const darkMode = useSignal(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    if (typeof window !== 'undefined') {
+      const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      darkMode.value = mediaQuery.matches;
 
-    const updateMode = (event: MediaQueryListEvent) => {
-      darkMode.value = event.matches;
+      const updateMode = (event: MediaQueryListEvent) => {
+        darkMode.value = event.matches;
+        document.documentElement.dataset.theme = darkMode.value ? 'dark' : 'light';
+      };
+
+      mediaQuery.addEventListener('change', updateMode);
       document.documentElement.dataset.theme = darkMode.value ? 'dark' : 'light';
-    };
 
-    mediaQuery.addEventListener('change', updateMode);
-
-    // Set initial theme attribute
-    document.documentElement.dataset.theme = darkMode.value ? 'dark' : 'light';
-
-    return () => {
-      mediaQuery.removeEventListener('change', updateMode);
-    };
+      return () => {
+        mediaQuery.removeEventListener('change', updateMode);
+      };
+    }
   }, []);
 
   return darkMode;
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,21 +1,24 @@
 import { PageProps } from 'fresh/server.ts';
+import { useDarkMode } from '/lib/hooks/useDarkMode.ts';
 import { FreshContextState } from '/lib/types.ts';
 import { defaultDescription, defaultTitle } from '/lib/utils/misc.ts';
 import Header from '/components/Header.tsx';
-import { useDarkMode } from '/lib/hooks/useDarkMode.ts';
 
 interface Data {}
 
 export default function App({ route, Component, state }: PageProps<Data, FreshContextState>) {
   useDarkMode();
-
   return (
-    <html class='h-full'>
+    <html class='h-full bg-slate-800'>
       <head>
         <meta charset='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>{defaultTitle}</title>
         <meta name='description' content={defaultDescription} />
+        <meta name='author' content='Bruno Bernardino' />
+        <meta property='og:title' content={defaultTitle} />
+        <link rel='icon' href='/images/favicon.png' type='image/png' />
+        <link rel='apple-touch-icon' href='/images/favicon.png' />
         <link rel='stylesheet' href='/styles.css' />
       </head>
       <body class='h-full'>
